344f2c1d2dce278a63293cb2d16fa6e1bcaba0b3
jamshid1598 <dovurovjamshid982@gmail.com>
2023-10-31 14:12:50 +0500

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4>home</h4>
+
+    <p ref="p">Hi, this is {{ name }}, age {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="changeInfo">change info</button><br>
+    <input type="text" v-model="name">
+    <button @click="age++">increase age</button>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    const name = ref('Jamshid Dovurov')
+    const age = ref(25)
+
+    const p = ref(null)
+
+    const handleClick = () => {
+      console.log('you clicked me')
+      console.log(p)
+      p.value.classList.add('test-class')
+      p.value.textContent = "this is test text"
+    }
+    const changeInfo = () => {
+      console.log("change user info")
+      name.value = "Jasur Dovurov"
+      age.value = 26
+    }
+
+    return { name, age, handleClick, changeInfo, p }
   }
 }
 </script>
